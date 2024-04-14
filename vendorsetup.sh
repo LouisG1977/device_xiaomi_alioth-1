dbab596985b34ca6f357b06a9ef51a914e2d7575
Roxor-007 <koyande72@gmail.com>
2024-04-15 02:19:10 +0530

    alioth: Switch miuicamera repo [1/2]

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ rm -rf hardware/xiaomi;rm -rf packages/resources/devicesettings;
 git clone git@github.com:Roxor-007/hardware_xiaomi.git hardware/xiaomi;
 rm -rf hardware/xiaomi/megvii; rm -rf system/extras/su
 git clone git@github.com:Roxor-007/packages_resources_devicesettings packages/resources/devicesettings;
-git clone https://gitlab.com/dark.phnx12/android_vendor_xiaomi_alioth-miuicamera.git vendor/xiaomi/alioth-miuicamera
+git clone https://gitlab.com/Roxor-007/vendor_xiaomi_camera.git vendor/xiaomi/camera
 mkdir prebuilts/clang/host/linux-x86/clang-neutron; cd prebuilts/clang/host/linux-x86/clang-neutron; 
 curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman";
 chmod +x antman; ./antman -S; cd ../../../../../;
